f9971eb7f75b0d7f9c79335ea14349f671c66f8e
uturkarslan <ugurturkarslan@gmail.com>
2022-05-16 11:39:18 +0200

    Change license key to api key

--- infra/scripts/deploy_apps.sh
+++ infra/scripts/deploy_apps.sh
@@ -212,5 +212,5 @@ helm upgrade ${fifth[name]} \
     --create-namespace \
     --namespace ${fifth[namespace]} \
     --set dockerhubName=$DOCKERHUB_NAME \
-    --set newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
+    --set newRelicApiKey=$NEWRELIC_API_KEY \
     ../charts/fifth
