0ec545769081ee312e3c412a0891574c33cf15cd
uturkarslan <ugurturkarslan@gmail.com>
2022-06-09 16:01:45 +0200

    Rename

--- infra/scripts/02_deploy_apps.sh
+++ infra/scripts/02_deploy_apps.sh
@@ -52,15 +52,15 @@ zipkinexporter["namespace"]="third"
 ### Build & Push ###
 ####################
 
-# Proxy
-echo -e "\n--- Proxy ---\n"
-docker build \
-    --build-arg newRelicAppName=${proxy[name]} \
-    --build-arg newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
-    --tag "${DOCKERHUB_NAME}/${proxy[name]}" \
-    "../../apps/${proxy[name]}/."
-docker push "${DOCKERHUB_NAME}/${proxy[name]}"
-echo -e "\n------\n"
+# # Proxy
+# echo -e "\n--- Proxy ---\n"
+# docker build \
+#     --build-arg newRelicAppName=${proxy[name]} \
+#     --build-arg newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
+#     --tag "${DOCKERHUB_NAME}/${proxy[name]}" \
+#     "../../apps/${proxy[name]}/."
+# docker push "${DOCKERHUB_NAME}/${proxy[name]}"
+# echo -e "\n------\n"
 
 # First
 echo -e "\n--- First ---\n"
@@ -97,32 +97,32 @@ docker push "${DOCKERHUB_NAME}/${zipkinexporter[name]}"
 echo -e "\n------\n"
 #########
 
-################
-### Newrelic ###
-################
-echo "Deploying Newrelic ..."
+# ################
+# ### Newrelic ###
+# ################
+# echo "Deploying Newrelic ..."
 
-kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/px.dev_viziers.yaml && \
-kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/olm_crd.yaml && \
-helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && \
-kubectl create namespace newrelic ; helm upgrade --install newrelic-bundle newrelic/nri-bundle \
-    --wait \
-    --debug \
-    --set global.licenseKey=$NEWRELIC_LICENSE_KEY \
-    --set global.cluster=$aksName \
-    --namespace=newrelic \
-    --set newrelic-infrastructure.privileged=true \
-    --set global.lowDataMode=true \
-    --set ksm.enabled=true \
-    --set kubeEvents.enabled=true \
-    --set prometheus.enabled=true \
-    --set logging.enabled=true \
-    --set newrelic-pixie.enabled=true \
-    --set newrelic-pixie.apiKey=$PIXIE_API_KEY \
-    --set pixie-chart.enabled=true \
-    --set pixie-chart.deployKey=$PIXIE_DEPLOY_KEY \
-    --set pixie-chart.clusterName=$aksName
-#########
+# kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/px.dev_viziers.yaml && \
+# kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/olm_crd.yaml && \
+# helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && \
+# kubectl create namespace newrelic ; helm upgrade --install newrelic-bundle newrelic/nri-bundle \
+#     --wait \
+#     --debug \
+#     --set global.licenseKey=$NEWRELIC_LICENSE_KEY \
+#     --set global.cluster=$aksName \
+#     --namespace=newrelic \
+#     --set newrelic-infrastructure.privileged=true \
+#     --set global.lowDataMode=true \
+#     --set ksm.enabled=true \
+#     --set kubeEvents.enabled=true \
+#     --set prometheus.enabled=true \
+#     --set logging.enabled=true \
+#     --set newrelic-pixie.enabled=true \
+#     --set newrelic-pixie.apiKey=$PIXIE_API_KEY \
+#     --set pixie-chart.enabled=true \
+#     --set pixie-chart.deployKey=$PIXIE_DEPLOY_KEY \
+#     --set pixie-chart.clusterName=$aksName
+# #########
 
 ##########################
 ### Ingress Controller ###
@@ -165,27 +165,27 @@ helm upgrade ${proxy[name]} \
     --set dockerhubName=$DOCKERHUB_NAME \
     "../charts/${proxy[name]}"
 
-appIdOfProxy=$(curl -X GET 'https://api.eu.newrelic.com/v2/applications.json' \
-    -H "Api-Key:${NEWRELIC_API_KEY}" \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    | jq -r '.applications[] | select(.name==''"'${proxy[name]}'"'') | .id')
+# appIdOfProxy=$(curl -X GET 'https://api.eu.newrelic.com/v2/applications.json' \
+#     -H "Api-Key:${NEWRELIC_API_KEY}" \
+#     -H "Content-Type: application/json" \
+#     -H "Accept: application/json" \
+#     | jq -r '.applications[] | select(.name==''"'${proxy[name]}'"'') | .id')
 
-timestampOfProxy=$(date -u +%Y-%m-%dT%H:%M:%SZ)
-curl -X POST "https://api.eu.newrelic.com/v2/applications/$appIdOfProxy/deployments.json" \
-     -i \
-     -H "Api-Key:${NEWRELIC_API_KEY}" \
-     -H "Content-Type: application/json" \
-     -d \
-    '{
-        "deployment": {
-            "revision": "1.0.0",
-            "changelog": "Initial deployment",
-            "description": "Deploy the proxy app.",
-            "user": "datanerd@example.com",
-            "timestamp": "'"${timestampOfProxy}"'"
-        }
-    }'
+# timestampOfProxy=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+# curl -X POST "https://api.eu.newrelic.com/v2/applications/$appIdOfProxy/deployments.json" \
+#      -i \
+#      -H "Api-Key:${NEWRELIC_API_KEY}" \
+#      -H "Content-Type: application/json" \
+#      -d \
+#     '{
+#         "deployment": {
+#             "revision": "1.0.0",
+#             "changelog": "Initial deployment",
+#             "description": "Deploy the proxy app.",
+#             "user": "datanerd@example.com",
+#             "timestamp": "'"${timestampOfProxy}"'"
+#         }
+#     }'
 #########
 
 # First
